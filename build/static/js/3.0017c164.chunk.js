cbb358266e73dc40f02531338b789b8842c28540
anujeshyadav <anujeshdevelopersveltosest11@gmail.com>
2024-03-02 23:05:33 +0530

    updated

--- build/static/js/3.0017c164.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[3],{835:function(a,s,d){}}]);
-//# sourceMappingURL=3.0017c164.chunk.js.map
--- build/static/js/374.f9e23616.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[374],{2549:function(a,s){}}]);
-//# sourceMappingURL=374.f9e23616.chunk.js.map
